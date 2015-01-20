43f0e9a01ebda0cb23a37398d8dbcca7deac10f5
Gabriel C <Gabriel.Case@RACKSPACE.COM>
2015-01-19 23:02:09 -0600

    Added failing tests, code needs to be modified to make them pass

---
+++ string_helper.rb
@@ -0,0 +1,5 @@
+class StringHelper
+  def is_permutation?(first_string, second_string)
+  return true
+  end
+end
--- test/application_test.rb
+++ test/application_test.rb
@@ -1,2 +1,30 @@
-require_relative '../test_helper.rb'
-p 'test loaded'
+require_relative '../test_helper'
+require_relative '../string_helper'
+
+class TestStringHelper < MiniTest::Unit::TestCase
+  def setup
+    @string_helper = StringHelper.new
+  end
+
+  def test_strings_are_permutation
+    fail_message = 'is_permutation? is returning false but it should be true'
+    perm1 = 'cat'
+    perm2 = 'atc'
+    perm3 = 'tac'
+    perm4 = 'cta'
+
+    assert @string_helper.is_permutation?(perm1, perm2), fail_message
+    assert @string_helper.is_permutation?(perm3, perm4), fail_message
+  end
+
+  def test_strings_are_not_permutation
+    fail_message = 'is_permutation? is returning true but it should be false'
+    perm1 = 'cat'
+    perm2 = 'cad'
+    perm3 = 'will'
+    perm4 = 'willie'
+
+    assert !(@string_helper.is_permutation?(perm1, perm2)), fail_message
+    assert !(@string_helper.is_permutation?(perm3, perm4)), fail_message
+  end
+end
