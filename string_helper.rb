51faa792e7d1fa1970d691e70e4ee5fa0b42b718
Arthi Vikram <arth8325@mtlmnp0ymg3qn.rackspace.corp>
2015-02-02 15:22:13 -0500

    sample message

--- string_helper.rb
+++ string_helper.rb
@@ -1,5 +1,12 @@
 class StringHelper
   def is_permutation?(first_string, second_string)
-  fail('is_permutation not implemented')
+  # fail('is_permutation not implemented')
+  str1 = first_string.downcase.chars
+  str2 = second_string.downcase.chars
+  if str1.sort == str2.sort
+  	return true
+  else 
+  	return false
+  end
   end
 end
