b88d66cfddbbe21873c2142f393080ceecf667b6
Arthi Vikram <arth8325@mtlmnp0ymg3qn.rackspace.corp>
2015-02-02 15:59:12 -0500

    test

--- string_helper.rb
+++ string_helper.rb
@@ -1,6 +1,5 @@
 class StringHelper
   def is_permutation?(first_string, second_string)
-  # fail('is_permutation not implemented')
   str1 = first_string.downcase.chars
   str2 = second_string.downcase.chars
   if str1.sort == str2.sort
