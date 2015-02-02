554e9144f7d300c69c965758abd62ba294e7e6e6
Deonna Pitchford <deonna.pitchford@mmt01gfd58.rackspace.corp>
2015-02-02 13:27:34 -0600

    fixed the is_permutation? method

--- string_helper.rb
+++ string_helper.rb
@@ -1,5 +1,8 @@
 class StringHelper
   def is_permutation?(first_string, second_string)
-  fail('is_permutation not implemented')
+	first_string = first_string.downcase.chars.sort
+  	second_string = second_string.downcase.chars.sort
+	
+	first_string == second_string
   end
 end
